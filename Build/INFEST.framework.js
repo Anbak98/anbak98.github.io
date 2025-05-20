11bf8b88d54d72ef0251369c1f9023010cd1778b
anbak98 <anbak98@gmail.com>
2025-05-20 15:34:18 +0900

    feat: new web building

--- Build/INFEST.framework.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:81f7b6959757cc87c3face652c34aa02d47bc5ee6c0de25147bd52615378977c
-size 432423
---
+++ Build/INFEST_0520_WEB.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cb3f8d173fb3e32d78e163339b0987e92082c0a68a6525202037c488a8b379c9
+size 445040
